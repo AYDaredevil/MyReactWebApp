f270c071d8c14afbe32cb841ee5efaa18e07b45c
Amit Yadav <ybhupendra.singh.mat16@itbhu.ac.in>
2021-01-25 12:56:39 +0530

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
-import './App.css';
+import Menu from "./components/MenuComponent"
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
       <div className="container">
-      <NavbarBrand>Amit Yadav</NavbarBrand>
+      <NavbarBrand>Amit's  Restaurant</NavbarBrand>
       </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
